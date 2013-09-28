32476bf9ca15cdfcb8645a95d67514ef559aaed7
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-09-28 15:17:15 +0200

    Fix compilation warnings.

--- spn.c
+++ spn.c
@@ -4,18 +4,19 @@
  * We have four rounds with 5 subkey mixing steps.
  */
 #include<stdio.h>
+#include<unistd.h>
 #include<assert.h>
 
 #define BUF_SIZE 256
 
-const char sbox_matrix[0x0f] = {
+const char sbox_matrix[0x10] = {
     0x0e, 0x04, 0x0d, 0x01,
     0x02, 0x0f, 0x0b, 0x08,
     0x03, 0x0a, 0x06, 0x0c,
     0x05, 0x09, 0x00, 0x07
 };
 
-char sbox(char input) {
+char sbox(uint8_t input) {
     assert((input & 0xf0) == 0x00);
 
     return sbox_matrix[input];
